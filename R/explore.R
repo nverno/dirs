10529c4096c2c25c84bcc3428fb7c1ac8617b9f0
Noah Peart <noah.v.peart@gmail.com>
2015-12-10 01:08:34 -0500

    explore

--- R/explore.R
+++ R/explore.R
@@ -3,6 +3,7 @@
 ##' @import shinyTree
 ##' @param path path to directory to visualize
 ##' @param files keep only these files in nested list (default to NULL)
+##' @export
 explore_dir <- function(path, files=NULL) {
   if (!file.exists(path)) stop("path is not a valid directory.")
   lst <- nest_dir(path)
